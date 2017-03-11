4bf75ae9d11ba767c63b4ed747bd845b1cdb7fec
Piotr <piotrw9@gmail.com>
2017-03-11 23:05:19 +0100

    FlightController.cs: speed input added

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 public class FlightController : MonoBehaviour {
 
-    public float speed = 0.5f;
+    public float speed = 50f;
+    public float maxSpeed = 500f;
+    public float acceleration = 40f;
     public float horizontalRotationSpeed = 10f;
     public float verticalRotationSpeed = 10f;
-	
-	void Update () {
-        transform.Translate(-transform.right * speed * Time.deltaTime, Space.World);
-        HandleInput();
+    public float turningSpeed = 5f;
+
+    private float speedChange;
+
+    void Update () {
+        HandleTurningInput();
+        HandleSpeedInput();
     }
 
-    void HandleInput() {
+    void HandleTurningInput() {
         float vertical = Input.GetAxis("Horizontal") * horizontalRotationSpeed;
         float horizontal = Input.GetAxis("Vertical") * verticalRotationSpeed;
-        transform.Rotate(vertical, 0, horizontal);
+        float turning = Input.GetAxis("Turning") * turningSpeed;
+        transform.Rotate(vertical, turning, horizontal);
+    }
+
+    void HandleSpeedInput() {
+        HandleAcceleration();
+        HandleBraking();
+        HandleSpeedChange();
+    }
+    
+    void HandleAcceleration() {
+        speedChange = acceleration * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && speed <= maxSpeed) {
+            float diff = maxSpeed - speed;
+            if (diff < acceleration) {
+                speedChange = diff;
+            }
+            speed += speedChange;
+        }
+    }
+
+    void HandleBraking() {
+        speedChange = acceleration * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftShift) && speed >= 0) {
+            if (speed < speedChange) {
+                speedChange = speed;
+            }
+            speed -= speedChange;
+        }
+    }
+
+    void HandleSpeedChange() {
+        transform.Translate(-transform.right * speed * Time.deltaTime, Space.World);
     }
 }
