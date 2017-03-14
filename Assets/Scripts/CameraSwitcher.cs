7e8b593521b652e0248faeee745a3e53d405e1c4
SkatingKwiatek <olinadgisel@gmail.com>
2017-03-14 20:30:36 +0100

    CameraSwitcher

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour {
+
+    public Camera shipCam;
+    public Camera playerCam;
+
+ 
+    void Start()
+    {
+        playerCam.enabled = true;
+        shipCam.enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            playerCam.enabled = !playerCam.enabled;
+            shipCam.enabled = !shipCam.enabled;
+        }
+    }
+}
