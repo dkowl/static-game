d25b684df8e0e92a9401c1f26099f1b0afbd2883
SkatingKwiatek <olinadgisel@gmail.com>
2017-03-16 18:01:49 +0100

    Telekineza yay jupi

---
+++ Assets/Scripts/Grabable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabable : MonoBehaviour {
+
+    public bool isGrabbed = false;
+    public Camera playerCam;
+    private Rigidbody rigidbody;
+    Vector3 offset;
+    Renderer renderer;
+    Color color;
+    
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        renderer = GetComponent<Renderer>();
+        color = renderer.material.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isGrabbed) {
+            color.a = 0.3f;
+            renderer.material.color = color;
+            offset = playerCam.transform.position + playerCam.transform.forward * 1.5f - transform.position;
+            rigidbody.velocity = offset*8f;
+        }
+    }
+
+    public void ShootHeldObject() {
+        rigidbody.velocity = playerCam.transform.forward * 15f;
+        color.a = 1f;
+        renderer.material.color = color;
+
+    }
+
+    public void LetHeldObjectGoLezkaSieWOkuKreci() {
+        color.a = 1f;
+        renderer.material.color = color;
+    }
+}
---
+++ Assets/Scripts/Telekenisis.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telekenisis : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, fwd, out hit, 10)) {
+            Grabable grabable = hit.collider.GetComponent<Grabable>();
+            if (grabable)
+            {
+                if (Input.GetKeyDown(KeyCode.Mouse0) && grabable.isGrabbed == false)
+                {
+                    grabable.isGrabbed = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.Mouse0) && grabable.isGrabbed == true)
+                {
+                    grabable.isGrabbed = false;
+                    grabable.ShootHeldObject();
+
+                }
+                else if (Input.GetKeyDown(KeyCode.Mouse1) && grabable.isGrabbed == true) {
+                    grabable.isGrabbed = false;
+                    grabable.LetHeldObjectGoLezkaSieWOkuKreci();
+                }
+
+            }
+        }
+    }
+}
+ 
